b51e9a31f33b04af8acc7cef2326b71839b6c9b3
Tu <tunguyen6464@gmail.com>
2025-05-26 14:53:39 -0400

    prototype commit 5

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const slidercover = document.getElementById('slidercover')
 const draggablecover = document.getElementById("draggablecover")
 const draggablecoversum = document.getElementById("draggablecover-sum")
 const strlenmeters = document.getElementById('L')
+const strlenlambda = document.getElementById('L-lambda')
 const medium = document.getElementById("wavemedium")
 const mediumreflected = document.getElementById("wavemedium-reflected")
 const mediumsum = document.getElementById("wavemedium-sum")
@@ -20,16 +21,16 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var totalstrlenmeters = 10 //m
+var totalstrlenmeters = 4 //m
 const gapdotdistance = 0.3 //rem, (dot width + dot gap)
 var divisions = 16
 var datapoints = 160
 var indexOfLastDot = datapoints - 1
 
-var a = 2
+var a = 3
 var f = 3.1416
 var omega = (2*Math.PI)*f
-var wavespeed = 10
+var delay = 10
 
 var loop = true
 var t = 0
@@ -83,6 +84,7 @@ function generateSliderCover() {
     slidercover.max = `${gapdotdistance+ 1 + (datapoints/2)*gapdotdistance}`
     slidercover.step = `${gapdotdistance}`
     strlenmeters.textContent = `${totalstrlenmeters}m`
+    strlenlambda.textContent = `${(f/Math.PI).toFixed(2)}λ`
 
     slidercover.addEventListener("input", () => {
         let value = slidercover.value   
@@ -93,16 +95,17 @@ function generateSliderCover() {
         draggablecoversum.style.width = `${value}rem`
         let len = ((indexOfLastDot+1)*(totalstrlenmeters/datapoints)).toFixed(2)
         strlenmeters.textContent = `${len}m`
+        strlenlambda.textContent = `${(len/totalstrlenmeters * (f/Math.PI)).toFixed(2)}λ`
 
         let round = datapoints/(divisions)
-        let num = Math.ceil((indexOfLastDot-(round/2)) / round) * round 
+        let num = Math.ceil((indexOfLastDot-(round/2)) / round) * round
         
         if (num < indexOfLastDot) {
             num = num/(datapoints/divisions)
             if (num % 2 === 0) {
                 for (const axis of axes) {
                     const ticks = axis.getElementsByTagName('n')
-                    ticks[num/2 - 1].style.visibility = 'hidden'
+                    ticks[num/2 - 1].style.opacity= '0'
                 }
             }
         }
@@ -111,7 +114,7 @@ function generateSliderCover() {
             if (num % 2 === 1) {
                 for (const axis of axes) {
                     const ticks = axis.getElementsByTagName('n')
-                    ticks[Math.floor(num/2) - 1].style.visibility = ''
+                    ticks[Math.floor(num/2) - 1].style.opacity = ''
                 }
             }
         }
@@ -128,19 +131,26 @@ async function animate() {
     var sine = 0
     var sine2 = 0
     var sum = 0
+    var k = 1 //damping
     let reflect = -1 //fixed end = -1, free end = 1
     
 
-
     while(loop == true) {
-        await sleep(wavespeed)
-        t++
-        sine = a*Math.sin((-2*Math.PI)*f*(t/1000)).toFixed(18) //18 decimal places were tested to give best accuracy
-        dots[0].style.translate = `0rem ${sine}rem`
-
+        await sleep(delay)
+        t += 1
+        //k += 0.005
+        sine = (a/k)*Math.sin((-2*Math.PI)*f*(t/1000)).toFixed(18) //18 decimal places were tested to give best accuracy
         sine2 = parseFloat(dots[indexOfLastDot].style.translate.slice(4,))
+
+        /* Another inteference
+        let sine0reflected = parseFloat(dots2[0].style.translate.slice(4,))
+        if (!isNaN(sine0reflected)) sine += (-0.7*sine0reflected)
+        */
+
+        dots[0].style.translate = `0rem ${sine}rem`
         dots2[indexOfLastDot].style.translate = `0rem ${reflect*sine2}rem`
 
+        
         for (let i = indexOfLastDot; i > 0; i--) {
             dots[i].style.translate = dots[i-1].style.translate
         }
@@ -177,35 +187,52 @@ controlWaveButton.addEventListener("click", () => {
 
 resetWaveButton.addEventListener("click", async () => {
     loop = false
-    await sleep(wavespeed + 100)
+    await sleep(delay + 100)
     medium.textContent = ''
     mediumreflected.textContent = ''
     mediumsum.textContent = ''
     t = 0
-    await sleep(wavespeed + 100)
+    await sleep(delay + 100)
     generateDots()
     controlWaveButton.textContent = "SEND WAVE";
 });
 
 const fslider = document.getElementById("f")
 const fbox = document.getElementById("f-box")
-const vslider = document.getElementById("v")
-const vbox = document.getElementById("v-box")
+const tslider = document.getElementById("t")
+const tbox = document.getElementById("t-box")
 
 fslider.addEventListener('input', (e) => {
-    fbox.value = e.target.value;
+    /*
+    fbox.textContent = 'f'
+    const sub = document.createElement('sub')
+    let harmonic = (e.target.value/Math.PI).toFixed(1)
+    console.log(harmonic)
+    if (harmonic % 1 != 0) {
+        harmonic = Math.floor(harmonic*2)
+    }
+    sub.textContent = `${harmonic}`
+    fbox.appendChild(sub)
+    */
+    fbox.textContent = `${(e.target.value/Math.PI).toFixed(1)} f`
+    const sub = document.createElement('sub')
+    sub.textContent = '0'
+    fbox.appendChild(sub)
     f = e.target.value
+    strlenlambda.textContent = `${(f/Math.PI).toFixed(2)}λ`
+    
 });
+/*
 fbox.addEventListener('keydown', (e) => {
     if (e.key === "Enter") vslider.value = e.target.value;
     f = e.target.value
 });
-
-vslider.addEventListener('input', (e) => {
-    vbox.value = e.target.value;
-    wavespeed = e.target.value
+*/
+tslider.addEventListener('input', (e) => {
+    tbox.value = e.target.value;
+    delay = 10/e.target.value 
 });
-vbox.addEventListener('keydown', (e) => {
-    if (e.key === "Enter") vslider.value = e.target.value;
-    wavespeed = e.target.value
+tbox.addEventListener('keydown', (e) => {
+    if (e.key === "Enter") tslider.value = e.target.value;
+    delay = 10/e.target.value 
 });
